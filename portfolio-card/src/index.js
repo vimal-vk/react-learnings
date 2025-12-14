a0fc8fae03f923b0a4bb7eb07c12d607b23f20e2
vimal-vk <vimal.baskar@zohocorp.com>
2025-12-14 20:17:09 +0530

    portfolio-card - simple app using components and props

---
+++ portfolio-card/src/index.js
@@ -0,0 +1,66 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const Avatar = () => {
+  return (
+    <img
+      className="avatar"
+      src="avatar.png"
+      alt="My Avatar"
+    />
+  );
+};
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Vimal kumar B</h1>
+      <p>Full Stack Web Developer, Working as a software engineer. Passionate on new technologies and continuous learning.</p>
+    </div>
+  );
+};
+
+const SkillList = () => {
+  return <div className="skill-list">
+    <Skill skill="HTML" emoji="🔥" color="red"/>
+    <Skill skill="CSS" emoji="🎨" color="green"/>
+    <Skill skill="JavaScript" emoji="💻" color="yellow"/>
+    <Skill skill="React" emoji="⚛️" color="cyan"/>
+    <Skill skill="Node.js" emoji="🌳" color="lightgreen"/>
+    <Skill skill="Python" emoji="🐍" color="#b700ffff"/>
+  </div>
+}
+
+const Skill = (props) => {
+  return (
+    <div className="skill" style={{backgroundColor: props.color}}>
+      <span>{props.emoji}</span>
+      <span>{props.skill}</span>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
